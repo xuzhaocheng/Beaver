03fd7d55883c9b301647bbbaa4108f4b9b6885c2
xuzhaocheng <zhaochengxu1991@gmail.com>
2014-11-05 14:42:32 +0800

    add photo zooming

--- Beaver/EditPhotoViewController.m
+++ Beaver/EditPhotoViewController.m
@@ -9,7 +9,7 @@
 #import "EditPhotoViewController.h"
 #import <AssetsLibrary/AssetsLibrary.h>
 
-@interface EditPhotoViewController ()
+@interface EditPhotoViewController () <UIScrollViewDelegate>
 
 @property (strong, nonatomic) UIImageView *imageView;
 @property (strong, nonatomic) UIImage *image;
@@ -26,13 +26,23 @@
 - (void)setImage:(UIImage *)image
 {
     self.imageView.image = image;
-    
     self.imageView.frame = CGRectMake(0, 0, image.size.width, image.size.height);
+    
+    [self configureImageScrollView];
+    if (self.view.window) {
+        [self updateUI];
+    }
 }
 
 - (UIImage *)image
 {
     return self.imageView.image;
+}
+
+- (void)setImageScrollView:(UIScrollView *)imageScrollView
+{
+    _imageScrollView = imageScrollView;
+    [self configureImageScrollView];
 }
 
 - (UIImageView *)imageView
@@ -56,15 +66,69 @@
     [self.imageScrollView addSubview:self.imageView];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self updateUI];
 }
 
 
+#pragma mark - Helpers
+
 - (void)updateUI
 {
+    [self centeredFrame:self.imageView forScrollView:self.imageScrollView];
+}
+
+- (void)configureImageScrollView
+{
+    if (self.imageScrollView) {
+        CGRect bounds = self.imageScrollView.bounds;
+        CGFloat xScale = bounds.size.width / self.image.size.width;
+        CGFloat yScale = bounds.size.height / self.image.size.height;
+        
+        CGFloat minScale = MIN(xScale, yScale);
+        CGFloat maxScale = 3;
+        self.imageScrollView.minimumZoomScale = minScale;
+        self.imageScrollView.maximumZoomScale = maxScale;
+        self.imageScrollView.zoomScale = minScale;
+        self.imageScrollView.contentSize = self.imageScrollView.bounds.size;
+    }
+}
+
+- (void)centeredFrame:(UIView *)view forScrollView:(UIScrollView *)scrollView
+{
+    CGRect frameToCenter = view.frame;
+    CGSize boundsSize = scrollView.bounds.size;
+    UIEdgeInsets contentInsets = scrollView.contentInset;
     
+    boundsSize.width = boundsSize.width - contentInsets.left - contentInsets.right;
+    boundsSize.height = boundsSize.height - contentInsets.top - contentInsets.bottom - self.toolsScrollView.bounds.size.height;
+    
+    if (frameToCenter.size.width < boundsSize.width) {
+        frameToCenter.origin.x = (boundsSize.width - frameToCenter.size.width) / 2.0;
+    } else {
+        frameToCenter.origin.x = 0.f;
+    }
+    
+    if (frameToCenter.size.height < boundsSize.height) {
+        frameToCenter.origin.y = (boundsSize.height - frameToCenter.size.height) / 2.0;
+    } else {
+        frameToCenter.origin.y = 0;
+    }
+    
+    view.frame = frameToCenter;
+}
+
+#pragma mark - UIScrollViewDelegate
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return self.imageView;
+}
+
+- (void)scrollViewDidZoom:(UIScrollView *)scrollView
+{
+    [self centeredFrame:self.imageView forScrollView:scrollView];
 }
 
 /*
